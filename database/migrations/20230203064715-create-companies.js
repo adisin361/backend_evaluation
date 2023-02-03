19b252063f45935ad6d88ca850c5a4cd87d4eacb
Aditya Sinha <adityasinha361@gmail.com>
2023-02-03 12:23:55 +0530

    feat: add company and sector models to database

---
+++ database/migrations/20230203064715-create-companies.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('companies', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      ceo: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      score: {
+        type: Sequelize.NUMBER
+      },
+      sector_id: {
+        type: Sequelize.NUMBER
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('companies');
+  }
+};

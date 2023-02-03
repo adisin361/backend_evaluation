100b0cb0ab15f24cd841782c6607c360fce0bc01
Aditya Sinha <adityasinha361@gmail.com>
2023-02-03 15:05:41 +0530

    fix: add proper router in the server

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const companyRouter = require('./src/routes/companyRoutes');
 
 const PORT = 5050;
 
 const app = express();
 app.use(express.json());
+
+app.use('/api', companyRouter.router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

029c6f6a87670f9c613f6c9d796668bab2c0a1c1
Aditya Sinha <adityasinha361@gmail.com>
2023-02-03 14:40:22 +0530

    feat: add services to read the csv and carry out functionalities

---
+++ src/services/companyService.js
@@ -0,0 +1,83 @@
+const { companies, sectors } = require('../../database/models');
+const utils = require('../../src/utils');
+const { getCompanyUrl, getSectorUrl } = require('../helper');
+
+const axios = require('axios');
+const fetchData = async ({ urlLink }) => {
+  const csv = await axios.get(urlLink);
+  const lines = utils.readDataByLine(csv);
+
+  lines.forEach(async (line, index) => {
+    if (index === 0) {
+      return;
+    }
+    const data = utils.splitString(line);
+    const companyData = await axios.get(`${getCompanyUrl}${data[0]}`);
+    const company = {
+      id: companyData.id,
+      name: companyData.name,
+      ceo: companyData.ceo,
+      description: companyData.description,
+    };
+    await companies.create(company);
+  });
+
+  lines.forEach(async (line, index) => {
+    if (index === 0)
+      return;
+    const data = utils.splitString(line);
+    const sectorData = await axios.get(`${getSectorUrl}${data[1]}`);
+    const sector = {
+      name: data[1],
+    };
+
+    const newSector = await sectors.create(sector);
+    console.log(newSector);
+    sectorData.data.forEach(async (secData) => {
+      const score = ((Number(secData.performanceIndex[0].value) * 10) + (Number(secData.performanceIndex[1].value) / 10000) + (Number(secData.performanceIndex[2].value) * 10) + Number(secData.performanceIndex[3].value)) / 4;
+
+      await companies.update({ sector_id: newSector.data.id, score: score }, {
+        where: {
+          id: secData.companyId
+        }
+      });
+    });
+  });
+
+  return companies.findAll();
+};
+
+module.exports = {
+  fetchData
+};
+
+
+
+
+
+
+
+
+
+
+
+const getCompanyById = async (id) => {
+  return await companies.findOne({
+    where: {
+      id: id
+    }
+  });
+};
+
+const getCompaniesBySector = async (sector_id) => {
+  return await companies.findAll({
+    where: {
+      sector_id: sector_id
+    }
+  });
+};
+
+module.exports = {
+  getCompanyById,
+  getCompaniesBySector
+};

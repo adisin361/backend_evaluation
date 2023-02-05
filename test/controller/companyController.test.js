0cbebe9a2bd767902f58a378ff7637af258e17c1
Aditya Sinha <adityasinha361@gmail.com>
2023-02-06 02:39:08 +0530

    chore: add tests for update controller

--- test/controller/companyController.test.js
+++ test/controller/companyController.test.js
@@ -1,11 +1,13 @@
 const companyController = require('../../src/controllers/companyController');
 const companyService = require('../../src/services/companyService');
 
-describe('Company Utils Test Cases', () => {
+describe('Company controller test cases', () => {
   it('Should return company depending upon its id', async () => {
-    jest.spyOn(companyService, 'save').mockResolvedValue([
+    jest.spyOn(companyService, 'fetchData').mockResolvedValue([
       {
-        id: 1
+        id: '24ca0568-d946-4c14-a0d7-eb81295b7a9e',
+        name: 'D-Mart',
+        score: 19.569474999999997
       }
     ]);
     const mockReq = {
@@ -17,19 +19,22 @@ describe('Company Utils Test Cases', () => {
       json: jest.fn()
     };
 
-    await companyController.getCompanyById(mockReq, mockRes);
-    expect(mockRes.status).toBeCalledWith(200);
+    await companyController.save(mockReq, mockRes);
+    expect(mockRes.status).toBeCalledWith(201);
     expect(mockRes.json).toBeCalledWith(
       [
         {
-          id: 1
+          id: '24ca0568-d946-4c14-a0d7-eb81295b7a9e',
+          name: 'D-Mart',
+          score: 19.569474999999997
         }
       ]
     );
   });
 
+
   it('Should return error', async () => {
-    jest.spyOn(companyService, 'save').mockRejectedValue(new Error('Internal Server error!!'));
+    jest.spyOn(companyService, 'fetchData').mockRejectedValue(new Error('Internal Server error!!'));
 
     const mockReq = {};
 
@@ -43,6 +48,61 @@ describe('Company Utils Test Cases', () => {
     expect(mockRes.status).toBeCalledWith(500);
     expect(mockRes.json).toBeCalledWith({
       error: 'Internal Server error!!'
+    });
+  });
+
+  it('should return updated company details of the given id', async () => {
+    jest.spyOn(companyService, 'updateCompanyDetails').mockResolvedValue([
+      {
+        id: 'c144e397-bef9-4aa1-aef4-842f4da44f9c',
+        name: 'Ebay',
+        ceo: 'Randall Pagac',
+      }
+    ]
+    );
+
+    const mockReq = {
+      id: jest.fn(),
+      body: jest.fn(),
+      query: jest.fn()
+    };
+
+    const mockRes = {
+
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+
+    };
+    await companyController.updateCompany(mockReq, mockRes);
+    expect(mockRes.status).toBeCalledWith(200);
+    expect(mockRes.json).toBeCalledWith(
+      [
+        {
+          id: 'c144e397-bef9-4aa1-aef4-842f4da44f9c',
+          name: 'Ebay',
+          ceo: 'Randall Pagac',
+        }
+      ]
+    );
+  });
+  it('should return error', async () => {
+    jest.spyOn(companyService, 'updateCompanyDetails').mockRejectedValue(new Error('Cannot read properties of undefined (reading \'id\')'));
+
+    const mockReq = {
+      id: jest.fn(),
+      body: jest.fn()
+    };
+
+    const mockRes = {
+
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+
+    };
+    await companyController.updateCompany(mockReq, mockRes);
+    expect(mockRes.status).toBeCalledWith(500);
+    expect(mockRes.json).toBeCalledWith({
+      error: 'Cannot read properties of undefined (reading \'id\')'
     });
   });
 });

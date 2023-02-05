53517b1b67a76436e609fd7db9ed612ea71318e1
Aditya Sinha <adityasinha361@gmail.com>
2023-02-06 02:53:45 +0530

    feat: add route for updation

--- src/routes/companyRoutes.js
+++ src/routes/companyRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const middlewares = require('../middleware/validateBody');
+const middlewares2 = require('../middleware/idValidation');
 const companyControllers = require('../controllers/companyController');
 const router = express.Router();
 
-router.route('/save').post(middlewares, companyControllers.save);
+router
+  .route('/save')
+  .post(middlewares, companyControllers.save);
+router
+  .route('/update')
+  .patch(middlewares2, companyControllers.updateCompany);
 
 module.exports = router;
 
